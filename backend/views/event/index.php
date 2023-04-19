26804bf84783c62deaba7eebbcd098e7313d5406
haphat <phattvt01@gmail.com>
2023-04-19 16:18:09 +0700

    video 32 -33

---
+++ backend/views/event/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use backend\models\Event;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var backend\models\EventSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Events');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="event-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Event'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= \yii2fullcalendar\yii2fullcalendar::widget(array(
+        'events' => $events
+    )); ?>
+
+</div>

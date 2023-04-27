bdcbbb1914016052a364c342f9a60c413efa1dbf
haphat <phattvt01@gmail.com>
2023-04-27 09:08:52 +0700

    video 49 setting and getting cookie

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -28,7 +28,7 @@ class SiteController extends Controller
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index'],
+                        'actions' => ['logout', 'index', 'set-cookie', 'show-cookie'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -53,6 +53,23 @@ class SiteController extends Controller
                 'class' => \yii\web\ErrorAction::class,
             ],
         ];
+    }
+
+    public function actionSetCookie()
+    {
+        $cookie = new yii\web\Cookie([
+            'name' => 'test',
+            'value' => 'test cookie value'
+    ]);
+        Yii::$app->getResponse()->getCookies()->add($cookie);
+    }
+
+    public function actionShowCookie()
+    {
+        if( Yii::$app->getRequest()->getCookies()->has('test'))
+        {
+            print_r(Yii::$app->getRequest()->getCookies()->getValue('test'));
+        }
     }
 
     /**
